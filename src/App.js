e7227c41a50a1fa13033b025cf6e67eeaaf43798
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-02-23 10:56:10 +0300

    06. Уроки React JS (index.js, App, JSX)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hello world!! 
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"

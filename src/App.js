5b7ac397241630b242ae54dfc65bcfd40b3c0324
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-03-07 14:24:15 +0300

    lol

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Nav/Navbar';
 import Profile from './Components/Profile/Profile';
-
+import lol from './music/lol.mp3';
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
       <Profile />
+      <audio src={lol} autoPlay></audio>
     </div>
   );
 };
